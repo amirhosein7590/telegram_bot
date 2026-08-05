4db114341a8d306883d69cbc53f4671ee4fb8fdc
amirhosein <amirhosein7590@gmail.com>
2026-08-05 23:59:40 +0330

    add a port to run project because its project must to run in render.com

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
+import http from "node:http";
 import { bot } from "./app/bot";
 
-async function bootstrap() {
-  bot.start({
-    onStart(botInfo) {
-      console.log(`Bot started as @${botInfo.username}`);
-    },
-  });
-}
 
-bootstrap();
+http
+  .createServer((req, res) => {
+    if (req.url === "/health") {
+      res.writeHead(200);
+      res.end("OK");
+      return;
+    }
+
+    res.writeHead(404);
+    res.end();
+  })
+  .listen(3000);
+
+
+bot.start({
+  onStart(botInfo) {
+    console.log(
+      `Bot started as @${botInfo.username}`
+    );
+  },
+});
