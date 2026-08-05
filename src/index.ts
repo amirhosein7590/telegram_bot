137c7b979010d21a4711ffcaf9535df7cec1d86e
amirhosein <amirhosein7590@gmail.com>
2026-08-06 01:24:16 +0330

    add bot init that not needed in previous version (log pooling)

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,74 @@
 import http from "node:http";
 import { telegramWebhook } from "./webhook/telegram.webhook";
+import { bot } from "./app/bot";
+
 
 const PORT = Number(process.env.PORT) || 3000;
 
-http
-  .createServer((req, res) => {
-    if (req.method === "POST" && req.url === "/telegram") {
-      console.log("REQUEST:", req.method, req.url);
-      telegramWebhook(req, res);
 
-      return;
-    }
+async function bootstrap() {
 
-    if (req.method === "GET" && req.url === "/health") {
-      res.writeHead(200);
-      res.end("OK");
+  await bot.init();
 
-      return;
-    }
+  console.log(
+    `Bot initialized as @${bot.botInfo.username}`
+  );
 
-    res.writeHead(404);
-    res.end();
-  })
-  .listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+
+  http
+    .createServer((req, res) => {
+
+
+      console.log(
+        "REQUEST:",
+        req.method,
+        req.url
+      );
+
+
+      if (
+        req.method === "POST" &&
+        req.url === "/telegram"
+      ) {
+        telegramWebhook(req, res);
+        return;
+      }
+
+
+      if (
+        req.method === "GET" &&
+        req.url === "/health"
+      ) {
+
+        res.writeHead(200);
+        res.end("OK");
+
+        return;
+      }
+
+
+      res.writeHead(404);
+      res.end();
+
+    })
+    .listen(PORT, () => {
+
+      console.log(
+        `Server running on port ${PORT}`
+      );
+
+    });
+
+}
+
+
+bootstrap().catch((error) => {
+
+  console.error(
+    "Application failed:",
+    error
+  );
+
+  process.exit(1);
+
+});
